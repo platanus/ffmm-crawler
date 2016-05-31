c0094ffcdfb723fb2088a35b069ac6b861367380
Ignacio Baixas <iobaixas@gmail.com>
2016-05-31 16:42:23 -0400

    feat(valor_cuota_diaria): adds :fecha param support

--- app/navigators/valor_cuota_diaria.rb
+++ app/navigators/valor_cuota_diaria.rb
@@ -1,12 +1,24 @@
 class ValorCuotaDiaria < Crabfarm::BaseNavigator
   def run
-    base_url = 'http://www.aafm.cl/estadisticas_publico/valor_cuota_diaria.php?consulta=4&'
+    base_url = 'http://www.aafm.cl/estadisticas_publico/valor_cuota_diaria.php?'
 
-    params[:administradora] = '%' unless params.key? :administradora
-    params[:tipo] = '%' unless params.key? :tipo
-    params[:inversion] = 'N' unless params.key? :inversion
+    query = {
+      consulta: 4,
+      administradora: params.fetch(:administradora, '%'),
+      tipo: params.fetch(:tipo, '%'),
+      inversion: params.fetch(:inversion, 'N'),
+      anio: query_date.year,
+      mes: query_date.month,
+      dia: query_date.day
+    }
 
-    browser.goto base_url+params.to_query
+    browser.goto base_url + query.to_query
     reduce_with_defaults
+  end
+
+  private
+
+  def query_date
+    @query_date ||= params[:fecha] ? Date.parse(params[:fecha]) : Time.now
   end
 end
--- app/reducers/valor_cuota_diaria_reducer.rb
+++ app/reducers/valor_cuota_diaria_reducer.rb
@@ -5,14 +5,14 @@ class ValorCuotaDiariaReducer < Crabfarm::BaseReducer
     self.share_values = Array.new
     share_values_tr_list.elements.drop(1).each do |elem|
       row_data = elem.search("td")
-      next if row_data.count<6 || row_data[1].text.blank?
+      next if row_data.count < 6 || row_data[1].text.blank?
       share_value = ShareValue.new
       share_value.institution = row_data[0].text
       share_value.run = row_data[1].text
       share_value.fund = row_data[2].text
       share_value.serie = row_data[3].text
       share_value.value = row_data[5].text.delete('.').sub(',','.').to_f
-      share_value.date = "#{params[:anio]}-#{params[:mes]}-#{params[:dia]}"
+      share_value.date = (params[:fecha] || Time.now.to_date).to_s
       share_values << share_value
     end
   end
