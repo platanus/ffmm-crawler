9cffcde63378e51f3bc1bf72e51541aa42efe10e
Ignacio Baixas <iobaixas@gmail.com>
2016-05-31 16:34:00 -0400

    feat(valor_cuota_diaria): adds default values for parameters

--- app/navigators/valor_cuota_diaria.rb
+++ app/navigators/valor_cuota_diaria.rb
@@ -1,6 +1,11 @@
 class ValorCuotaDiaria < Crabfarm::BaseNavigator
   def run
     base_url = 'http://www.aafm.cl/estadisticas_publico/valor_cuota_diaria.php?consulta=4&'
+
+    params[:administradora] = '%' unless params.key? :administradora
+    params[:tipo] = '%' unless params.key? :tipo
+    params[:inversion] = 'N' unless params.key? :inversion
+
     browser.goto base_url+params.to_query
     reduce_with_defaults
   end
